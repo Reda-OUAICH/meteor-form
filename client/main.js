001d9adcbc4a28bab7702df89c3c5e1decf0e001
Réda Ouaich <reda.ouaich@hetic.net>
2018-12-11 14:29:16 +0100

    update

--- client/main.js
+++ client/main.js
@@ -13,10 +13,8 @@ if (Meteor.isClient) {
       var firstname = event.target.firstname.value;
       var lastname = event.target.lastname.value;
       var link = event.target.link.value;
-      console.log(firstname, lastname, link);
-      event.target.firstname.value = '';
-      event.target.lastname.value = '';
-      event.target.link.value = '';
+      var result = firstname && lastname && link;
+      console.log(result);
     }
   });
 }
