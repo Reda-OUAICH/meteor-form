b33d9e077aaf1cefa37fcd7d54615bac1441f293
Réda Ouaich <reda.ouaich@hetic.net>
2018-12-11 14:17:49 +0100

    adding template form

--- client/main.js
+++ client/main.js
@@ -4,8 +4,19 @@ import { Template } from 'meteor/templating';
 import './main.html';
 
 
-Template.form.onCreated(function helloOnCreated() {
-});
+if (Meteor.isClient) {
 
-Template.form.helpers({
-});
+  Template.form.events({
+
+    'submit form': function (event) {
+      event.preventDefault();
+      var firstname = event.target.firstname.value;
+      var lastname = event.target.lastname.value;
+      var link = event.target.link.value;
+      console.log(firstname, lastname, link);
+      event.target.firstname.value = '';
+      event.target.lastname.value = '';
+      event.target.link.value = '';
+    }
+  });
+}
