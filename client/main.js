f7571be7c297cd3c27691a6eecb3c349ab964156
Réda Ouaich <reda.ouaich@hetic.net>
2018-12-11 13:42:35 +0100

    adding miligramejs

--- client/main.js
+++ client/main.js
@@ -1,22 +1,16 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+
+Template.form.onCreated(function helloOnCreated() {
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.form.helpers({
 });
 
-Template.hello.events({
+Template.form.events({
   'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
   },
 });
